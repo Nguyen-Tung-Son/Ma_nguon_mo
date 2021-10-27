8235d0c0f49e2d408d464029647933a23570b34e
1821050999@student.humg.edu.vn <1821050999@student.humg.edu.vn>
2021-10-27 20:26:31 +0700

    first commit

---
+++ Manguonmo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Manguonmo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
